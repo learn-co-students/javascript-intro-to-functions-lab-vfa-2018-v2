83873bc872dcf836497eb658a1cdfdc4f66805b9
Dan Bardach <bardachd@xavier.edu>
2018-06-26 20:44:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string) {
+console.log(logShout); string.toUpperCase()
+}
+log(spy); hello.toUpperCase()
+
+function logWhisper(string){
+  console.log(logWhisper, hello); string.toLowerCase()
+}
+function sayHiToGrandma(string){
+  return string.toLowerCase()
+}
