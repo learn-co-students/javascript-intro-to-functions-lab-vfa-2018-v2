bc1f559e8fed2cb2187fa75ec2414896f49e91f7
Yuzuka Akasaka <yuzuka.akasaka@gmail.com>
2018-06-28 21:41:47 +0000

    Done.

--- index.js
+++ index.js
@@ -20,18 +20,19 @@ function logWhisper(string) {
 
 //5)//
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string);
-  return "I can't hear you!"
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";}
 }
 
 //6)//
 function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string);
-  return "YES INDEED!"
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";}
 }
 
 //7)//
 function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.");
-  return "I love you, too."
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
