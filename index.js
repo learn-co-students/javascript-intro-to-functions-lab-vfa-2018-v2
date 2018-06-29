ee099d059b1925efcd73ad9677f380dda5173373
Charles Beckman <charliejbeckman@gmail.com>
2018-06-29 21:22:18 +0000

    Done.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!";
     }
   
-  if (string==="I love you, Grandma") {
+  if (string === "I love you, Grandma.") {
     return "I love you, too.";
     }
 }
