9ac36be3beedb83e0bdbdfd0eaa9333fd40de1cb
Charles Beckman <charliejbeckman@gmail.com>
2018-06-29 21:20:28 +0000

    Done.

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   
-  if (string.toLowerCase() === true) {
+  if (string.toLowerCase() === string) {
     return 'I can\'t hear you!';
     } 
   
-  if (string.toUpperCase() === true) {
+  if (string.toUpperCase() === string) {
     return "YES INDEED!";
     }
   
