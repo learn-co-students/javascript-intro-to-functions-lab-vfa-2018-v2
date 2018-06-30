4e6ecd018c5941c08c0e410f41297723e21f77d2
Hayden Cohen <haydenjcohen@gmail.com>
2018-06-30 20:28:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(String) {
+  return String.toUpperCase()
+}
+
+function whisper(String) {
+  return String.toLowerCase()
+}
+
+function logShout(String) {
+  console.log() String.toUpperCase
+}
