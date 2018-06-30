192fb7a07be56b2a81f7f2654112d519cd37b75a
Dan Bardach <bardachd@xavier.edu>
2018-06-30 18:27:19 +0000

    Done.

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ function whisper(string){
   return string.toLowerCase()
 }
 function logShout(string) {
-console.log(logShout); string.toUpperCase()
+  console.log(string.toUpperCase())
 }
-log(spy); hello.toUpperCase()
-
-function logWhisper(string){
-  console.log(logWhisper, hello); string.toLowerCase()
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string){
-  return string.toLowerCase()
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
