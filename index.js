c0d6ebf72441e09e34c03afe4103221efac36ab3
Acacia Galle <acacia.galle@yahoo.com>
2018-06-30 13:32:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(STRING){
+  return STRING.toLowerCase()
+}
+
+function logShout(string)
+}
+
+function sayHiToGrandma(string){
+  if (typeof string === "string") {return "I can't hear you"; }
+}
