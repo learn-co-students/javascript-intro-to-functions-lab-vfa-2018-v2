99cfc3123b8260a4ba567aac39cc3390469b9b5b
Sarah Perry <sarahcatherineperry@gmail.com>
2018-07-01 01:51:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  var spy
+  spy= 'HELLO'
+  console.log(spy); string.toUpperCase;
+}
+
+function logWhisper(string) {
+  var spy 
+  spy = 'hello'
+  console.log(spy); string.toLowerCase;
+}
+// this is the grandma section
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }}
+  
+  { if (string.toUpperCase() === string)
+    return "YES INDEED!";
+  }
+  
+  { if (string === "I love you, Grandma.") 
+    return "I love you, too.";
+  }
+
+ 
+
+  
+
+
+
+
+
+
+
+
+
+
+
